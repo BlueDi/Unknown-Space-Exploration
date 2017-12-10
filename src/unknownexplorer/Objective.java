431a7a12629a74d2294fc50b1cbd34b6e2a1b54f
paulocosta33 <jigkiller5@gmail.com>
2017-12-10 00:12:49 +0000

    objective point fix

--- src/unknownexplorer/Objective.java
+++ src/unknownexplorer/Objective.java
@@ -1,5 +1,6 @@
 package unknownexplorer;
 
+import java.lang.reflect.Field;
 import java.util.Random;
 
 public class Objective {
@@ -7,13 +8,20 @@ public class Objective {
 	private double yObjective;
 
 	public Objective() {
-		Random r = new Random();
-		double[] randomNumbers = r.doubles(2, 0, 101).toArray();
-		xObjective = randomNumbers[0];
-		yObjective = randomNumbers[1];
+		//Random r = new Random();
+		//double[] randomNumbers = r.doubles(2, 0, 101).toArray();
+		xObjective = 0;// randomNumbers[0];
+		yObjective = 0;//randomNumbers[1];
 	}
 
 	public String toString() {
 		return "Wall, [" + xObjective + ", " + yObjective + "]";
+	}
+
+	public double getDeclaredField(String string) {
+		if (string == "xObjective"){
+			return xObjective;
+		}
+		else return yObjective;
 	}
 }
